33333a49ff9eb01c4f02b911c2b682e313b8b97e
KM <the.real.laplace@gmail.com>
2025-03-10 02:08:38 +0200

    chore(commit): force

--- src/for-points.js
+++ src/for-points.js
@@ -5,9 +5,10 @@ const sortBy$$$ = sortBy.$$$
 
 const getDist = (x) => x.dist
 
-const _nearestPoint = ([ x0, x1, y0, y1 ], [ x, y ]) => [
+const _nearestPoint = ([ x0, x1, y0, y1, z0, z1 ], [ x, y, z ]) => [
 	clamp(x, x0, x1),
 	clamp(y, y0, y1),
+	clamp(z, z0, z1),
 ]
 
 class Octree {
@@ -18,10 +19,11 @@ class Octree {
 	}
 
 	insert (point) {
-		const [ x, y ] = point
+		const [ x, y, z ] = point
 		let [
 			{ from: x0, to: x1 },
 			{ from: y0, to: y1 },
+			{ from: z0, to: z1 },
 		] = this._bounds
 
 		let node = this._tree
@@ -30,13 +32,20 @@ class Octree {
 		for (let depth = 0; depth < this._maxDepth; depth++) {
 			let next = node[index]
 			if (next === null) {
-				next = node[index] = new Array(4).fill(null)
+				next = node[index] = new Array(8).fill(null)
 			}
 			node = next
 			const xm = (x0 / 2) + (x1 / 2)
 			const ym = (y0 / 2) + (y1 / 2)
+			const zm = (z0 / 2) + (z1 / 2)
 
 			index = 0
+			if (z > zm) {
+				index += 4
+				z0 = zm
+			} else {
+				z1 = zm
+			}
 			if (y > ym) {
 				index += 2
 				y0 = ym
@@ -59,11 +68,9 @@ class Octree {
 	}
 
 	nearestNeighbor (target, fnDist) {
-		const solution = { point: null, dist: Infinity, culled: 0, calls: 0 }
+		const solution = { point: null, dist: Infinity }
 
-		const _nearestNeighbor = (node, [ x0, x1, y0, y1 ], depth) => {
-			solution.calls++
-
+		const _nearestNeighbor = (node, [ x0, x1, y0, y1, z0, z1 ], depth) => {
 			if (depth === this._maxDepth) {
 				for (const point of node) {
 					const dist = fnDist(point, target)
@@ -77,38 +84,51 @@ class Octree {
 
 			const xm = x0 / 2 + x1 / 2
 			const ym = y0 / 2 + y1 / 2
+			const zm = z0 / 2 + z1 / 2
 			const quadrants = [
-				[ x0, xm, y0, ym ],
-				[ xm, x1, y0, ym ],
-				[ x0, xm, ym, y1 ],
-				[ xm, x1, ym, y1 ],
+				[ x0, xm, y0, ym, z0, zm ],
+				[ xm, x1, y0, ym, z0, zm ],
+				[ x0, xm, ym, y1, z0, zm ],
+				[ xm, x1, ym, y1, z0, zm ],
+				[ x0, xm, y0, ym, zm, z1 ],
+				[ xm, x1, y0, ym, zm, z1 ],
+				[ x0, xm, ym, y1, zm, z1 ],
+				[ xm, x1, ym, y1, zm, z1 ],
 			]
 
-			let index = 0
-			if (target.y > ym) {
-				index += 2
-			}
-			if (target.x > xm) {
-				index += 1
+			let index
+			const [ x, y, z ] = target
+			if (x < x0 || x1 < x || y < y0 || y1 < y || z < z0 || z1 < z) {
+				index = -1
+			} else {
+				index = 0
+				if (z > zm) { index += 4 }
+				if (y > ym) { index += 2 }
+				if (x > xm) { index += 1 }
 			}
 
-			if (node[index]) { _nearestNeighbor(node[index], quadrants[index], depth + 1) }
+
+			if (index !== -1 && node[index]) {
+				_nearestNeighbor(node[index], quadrants[index], depth + 1)
+			}
 
 			const invocations = []
-			for (let i = 0; i < 4; i++) {
+			for (let i = 0; i < 8; i++) {
 				if (i === index || !node[i]) { continue }
 
 				const quadrant = quadrants[i]
 				const nearest = _nearestPoint(quadrant, target)
 				const lowerBound = fnDist(target, nearest)
-				if (lowerBound > solution.dist) { solution.culled++; continue }
+				if (lowerBound > solution.dist) { continue }
 
-				invocations.push({ dist: 0, child: node[i], quadrant })
+				invocations.push({ dist: lowerBound, child: node[i], quadrant })
 			}
 
 			sortBy$$$(invocations, getDist)
 
-			for (const { child, quadrant } of invocations) {
+
+			for (const { dist, child, quadrant } of invocations) {
+				if (dist > solution.dist) { continue }
 				_nearestNeighbor(child, quadrant, depth + 1)
 			}
 		}
@@ -118,8 +138,9 @@ class Octree {
 			const [
 				{ from: x0, to: x1 },
 				{ from: y0, to: y1 },
+				{ from: z0, to: z1 },
 			] = this._bounds
-			_nearestNeighbor(node, [ x0, x1, y0, y1 ], 0)
+			_nearestNeighbor(node, [ x0, x1, y0, y1, z0, z1 ], 0)
 		}
 
 		return solution
